df11becc7fabb47dc6d0b42d486386ec71b3536f
indy <bleubirb@users.noreply.github.com>
2024-03-12 22:30:51 -0700

    works?

--- Honk/src/BT.c
+++ Honk/src/BT.c
@@ -11,12 +11,14 @@ int main(void)
     BOARD_Init();
     Uart1_Init(9600);
     uint8_t data[128];
-    uint8_t data2[128];
-    strcpy((char *)data2, "please no more\n");
+    // uint8_t data3[128];
+    // strcpy((char *)data3, "please no more\n");
+    memset(data, 0, 128);
     while (1)
     {
         Uart1_rx(data, 128);
         printf("data: %s\n", data);
+        memset(data, 0, 128);
         Uart1_tx(data, strlen((char *)data));
         HAL_Delay(1000);
     }
