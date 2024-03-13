747d398f7dfc5cdd423035567984c415f357a59f
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2024-03-13 03:19:05 -0700

    this seems to be working?

--- Honk/src/BT.c
+++ Honk/src/BT.c
@@ -3,44 +3,242 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define UART_SPEED 9600
+#define BUFFER_SIZE 128
+#define DATA_SIZE 128
 
 #define HEAD 0xCC
 #define TAIL 0xB9
-#define END 0x0D0A
+#define END1 0x0D
+#define END2 0x0A
 
-typedef struct packet_struct {
+typedef struct packet_struct
+{
     uint8_t id;
-    uint8_t data[128];
+    uint8_t data[DATA_SIZE];
     uint8_t length;
     uint8_t checksum;
 } *packet_t;
+
+typedef struct buffer_struct
+{
+    packet_t buffer[BUFFER_SIZE];
+    int head;
+    int tail;
+    int full;
+    int empty;
+} *buffer_t;
+
+buffer_t buffer;
+packet_t tmp_packet;
+
+void init()
+{
+    buffer = (buffer_t)malloc(sizeof(struct buffer_struct));
+    buffer->head = 0;
+    buffer->tail = 0;
+    buffer->full = 0;
+    buffer->empty = 1;
+
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+        buffer->buffer[i] = (packet_t)malloc(sizeof(struct packet_struct));
+    }
+
+    tmp_packet = (packet_t)malloc(sizeof(struct packet_struct));
+}
 
 // each packet follows the format:
 // HEAD | LENGTH | ID | DATA | TAIL | CHECKSUM | END
 // where HEAD and TAIL are constant, LENGTH is the length of the data (not including the ID), ID is the packet ID, DATA is the packet data/payload, and CHECKSUM is the xor of all the bytes in the packet
 
-void packet_parser(packet_t *packet, uint8_t c)
+int packet_ready = 0;
+
+void packet_parser(packet_t packet, uint8_t c)
 {
     static uint8_t state = 0;
-    
+    static int index = 0;
+    // printf("%x %c\n", c, c);
+    switch (state)
+    {
+    case 0:
+        if (c == HEAD)
+        {
+            state = 1;
+        }
+        index = 0;
+        break;
+    case 1:
+        packet->length = c;
+        // printf("%d\n", packet->length);
+        state = 2;
+        break;
+    case 2:
+        packet->id = c;
+        // printf("%d\n", packet->id);
+        packet->checksum = c;
+        if (packet->length == 0)
+        {
+            state = 4;
+        // printf("data4\n");
+        }
+        else
+        {
+            state = 3;
+        // printf("data3\n");
+        }
+        break;
+    case 3:
+        packet->data[index] = c;
+        packet->checksum ^= c;
+        index++;
+        if (index == packet->length)
+        {
+            state = 4;
+        }
+        break;
+    case 4:
+        if (c == TAIL)
+        {
+            state = 5;
+        }
+        else
+        {
+            state = 0;
+        }
+        break;
+    case 5:
+        if (c == packet->checksum)
+        {
+            state = 6;
+        }
+        else
+        {
+            state = 0;
+        }
+        break;
+    case 6:
+        if (c == END1)
+        {
+            state = 7;
+        }
+        else
+        {
+            state = 0;
+        }
+        break;
+    case 7:
+        if (c == END2)
+        {
+            packet_ready = 1;
+            state = 0;
+        }
+        else
+        {
+            state = 0;
+        }
+        break;
+    default:
+        break;
+    }
 }
 
 int main(void)
 {
     BOARD_Init();
     Uart1_Init(9600);
-    uint8_t data[128];
-    memset(data, 0, 128);
+    init(&buffer, &tmp_packet);
+    uint8_t data[DATA_SIZE];
+    // uint8_t last_data[DATA_SIZE];
+    printf("init done\n");
     while (1)
     {
-        Uart1_rx(data, 128);                  // read data from bluetooth
-        printf("data: %s\n", data);           // print data to console
-        Uart1_tx(data, strlen((char *)data)); // send data to bluetooth
-        HAL_Delay(1000);
-        // if uart successfully reads data, clear the buffer
-        // this should not work, and should cause
-        Uart1_rx(data, 128);
+        if (Uart1_rx(data, DATA_SIZE) == 1)
+        {
+            // int start = 0;
+            // for (int i = 0; i < DATA_SIZE; i++)
+            // {
+            //     if (data[i] != last_data[i])
+            //     {
+            //         start = i;
+            //         break;
+            //     }
+            // }
+            // uint8_t c;
+
+            int start = 0;
+            for (int i = 0; i < DATA_SIZE; i++) {
+                if (data[i] == HEAD) {
+                    start = 0;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < DATA_SIZE; i++)
+            {
+                // c = data[i];
+                packet_parser(tmp_packet, data[(start + i) % DATA_SIZE]);
+                // printf("data[%d]: %x\n", i, data[i]);
+                // packet_parser(tmp_packet, data[i]);
+                if (packet_ready)
+                {
+                    packet_ready = 0;
+                    printf("packet ready\n");
+                    // copy packet to buffer
+                    buffer->buffer[buffer->tail]->id = tmp_packet->id;
+                    buffer->buffer[buffer->tail]->length = tmp_packet->length;
+                    for (int j = 0; j < tmp_packet->length; j++)
+                    {
+                        buffer->buffer[buffer->tail]->data[j] = tmp_packet->data[j];
+                    }
+                    buffer->tail = (buffer->tail + 1) % BUFFER_SIZE;
+                    if (buffer->tail == buffer->head)
+                    {
+                        buffer->full = 1;
+                    }
+                    buffer->empty = 0;
+                }
+            }
+        }
+
+        while (!buffer->empty) {
+            printf("buffer not empty\n");
+            printf("id: %d\n", buffer->buffer[buffer->head]->id);
+            // printf("length: %d\n", buffer->buffer[buffer->head]->length);
+            for (int i = 0; i < buffer->buffer[buffer->head]->length; i++)
+            {
+                printf("%c", buffer->buffer[buffer->head]->data[i]);
+                // printf("data[%d]: %d\n", i, buffer->buffer[buffer->head]->data[i]);
+            }
+            printf("\n");
+            buffer->head = (buffer->head + 1) % BUFFER_SIZE;
+            if (buffer->head == buffer->tail)
+            {
+                buffer->empty = 1;
+            }
+            buffer->full = 0;
+        }
+
+        // Uart1_rx(data, 128);
+        HAL_Delay(100);
     }
+    // uint8_t data[128];
+    // memset(data, 0, 128);
+    // while (1)
+    // {
+    //     // memset(data, 0, 128);
+    //     Uart1_rx(data, 128);                  // read data from bluetooth
+    //     printf("data: %s\n", data);           // print data to console
+    //     Uart1_tx(data, strlen((char *)data)); // send data to bluetooth
+    //     HAL_Delay(1000);
+    //     // if uart successfully reads data, clear the buffer
+    //     // this should not work, and should cause
+    //     Uart1_rx(data, 128);
+    //     for (int i = 0; i < 128; i++)
+    //     {
+    //         data[i] = 0;
+    //     }
+    // }
 }
--- Honk_Interface/interface.py
+++ Honk_Interface/interface.py
@@ -9,6 +9,10 @@ import random
 import Adafruit_BluefruitLE
 from Adafruit_BluefruitLE.services import UART
 
+HEAD = b"\xCC"
+TAIL = b"\xB9"
+END1 = b"\x0D"
+END2 = b"\x0A"
 
 # Get the BLE provider for the current platform.
 ble = Adafruit_BluefruitLE.get_provider()
@@ -17,12 +21,98 @@ devices = list()
 uarts = list()
 
 
+# def get_data_bytes(id: int, data: str):
+#     out = HEAD + id.to_bytes(1, "big") + data.encode("utf-8") + TAIL + END1 + END2
+#     return out + (b"\x00" * ((12 if len(out) < 128 else 256) - len(out)))
+
+
+# def get_data_bytes(id: int, data: bytes):
+#     out = HEAD + id.to_bytes(1, "big") + data + TAIL + END1 + END2
+#     return out + (b"\x00" * ((128 if len(out) < 128 else 256) - len(out)))
+
+
+# def get_data_bytes(data: tuple[int, bytes]):
+#     id, data = data
+#     out = HEAD + id.to_bytes(1, "big") + data + TAIL + END1 + END2
+#     # return out
+#     return out + (b"\x00" * ((128 if len(out) < 128 else 256) - len(out)))
+
+def get_data_bytes(data: tuple[int, str]):
+    id, data = data
+    cksum = id
+    for byte in data.encode("utf-8"):
+        cksum ^= byte
+    return HEAD + len(data).to_bytes(1, "big") + id.to_bytes(1, "big") + data.encode("utf-8") + TAIL + cksum.to_bytes(1, "big") + END1 + END2
+    # return out
+    # return out + (b"\xff" * ((26 if len(out) < 126 else 254) - len(out))) + END1 + END2
+
+
+tmp_id = None
+tmp_len = 0
+tmp_data = b""
+tmp_cksum = 0
+state = 0
+data_index = 0
+ready = False
+
+
+def parse_packet(input):
+    global tmp_id, tmp_len, tmp_data, tmp_cksum, state, data_index, ready
+    if state == 0:
+        if input == HEAD:
+            state = 1
+            tmp_id = None
+            tmp_len = 0
+            tmp_data = b""
+            tmp_cksum = 0
+            data_index = 0
+            ready = False
+    elif state == 1:
+        tmp_len = input
+        state = 2
+    elif state == 2:
+        tmp_id = input
+        tmp_cksum = tmp_id
+        if tmp_len == 0:
+            state = 4
+        else:
+            state = 3
+    elif state == 3:
+        tmp_data += input
+        tmp_cksum ^= input
+        data_index += 1
+        if data_index == tmp_len:
+            state = 4
+    elif state == 4:
+        if input == TAIL:
+            state = 5
+        else:
+            state = 0
+    elif state == 5:
+        if input == tmp_cksum:
+            state = 6
+        else:
+            state = 0
+    elif state == 6:
+        if input == END1:
+            state = 7
+        else:
+            state = 0
+    elif state == 7:
+        if input == END2:
+            ready = True
+            state = 0
+        else:
+            state = 0
+
+
 # Main function implements the program logic so it can run in a background
 # thread.  Most platforms require the main thread to handle GUI events and other
 # asyncronous events like BLE actions.  All of the threading logic is taken care
 # of automatically though and you just need to provide a main function that uses
 # the BLE provider.
 def main():
+    global tmp_id, tmp_data, ready
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     j = 0
     # Clear any cached data because both bluez and CoreBluetooth have issues with
@@ -84,28 +174,52 @@ def main():
         # uart = UART(device)
 
         # Write a string to the TX characteristic.
-        for uart in uarts:
-            uart.write(b"Hello World!\r\n")
-            print("Sent 'Hello world!' to the device.")
+        # for uart in uarts:
+        #     uart.write(b"Hello World!\r\n")
+        #     print("Sent 'Hello world!' to the device.")
+
+        in_queues = [list() for _ in uarts]
+        out_queues = [list() for _ in uarts]
+        for i in range(len(out_queues)):
+            out_queues[i].append((i, "Hello World!\n"))
+            uarts[i].write("hello, world!\n".encode("utf-8"))
 
         while True:
             for i, uart in enumerate(uarts):
+                print("UART: {0}".format(i))
                 # if random.random() < 0.5:
                 #     ts = datetime.datetime.now().isoformat()
                 # uart.write(b"hi there\r\n")
-                uart.write(b"%s\r\n" % alphabet[j].encode('utf-8'))
-                    # uart.write(b"%s\r\n" % ts.encode('utf-8'))
-                    # print("Sent '{1}' to UART {0}.".format(i, ts))
+                # uart.write(b"%s\r\n" % alphabet[j].encode('utf-8'))
+                # uart.write(b"%s\r\n" % ts.encode('utf-8'))
+                # print("Sent '{1}' to UART {0}.".format(i, ts))
                 # Now wait up to one minute to receive data from the device.
                 # print('Waiting up to 60 seconds to receive data from the device...')
-                received = uart.read(timeout_sec=5)
+                received = uart.read(timeout_sec=1)
                 if received is not None:
                     # Received data, print it out.
-                    print("Received: {0}, UART: {1}".format(received, i))
+                    print("Received: {0}".format(received))
+                    for byte in received:
+                        parse_packet(byte)
+                        if ready:
+                            in_queues[i].append((tmp_id, tmp_data))
+                            ready = False
                 else:
                     # Timeout waiting for data, None is returned.
                     print("Received no data on UART {0}!".format(i))
                     # break
+
+                while len(in_queues[i]) > 0:
+                    id, data = in_queues[i].pop(0)
+                    print("Received: {0}: {1}, UART: {2}".format(id, data, i))
+
+                out_queues[i].append((i, f"{alphabet[j]}\n"))
+
+                if len(out_queues[i]) > 0:
+                    print(get_data_bytes(out_queues[i][0]), len(get_data_bytes(out_queues[i][0])))
+                    uart.write(get_data_bytes(out_queues[i].pop(0)))
+                    print("Sent data to UART {0}.".format(i))
+
             j += 1
             if j == 26:
                 j = 0
